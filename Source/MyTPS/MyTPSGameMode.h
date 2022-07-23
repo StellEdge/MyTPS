65519ac1d3f44f16465cb8f9f214164ecad44bbd
Stelledge <1030110910@qq.com>
2022-07-23 22:53:10 +0800

    First commit

---
+++ Source/MyTPS/MyTPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyTPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMyTPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMyTPSGameMode();
+};
+
+
+
